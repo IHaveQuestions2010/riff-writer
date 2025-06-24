402b708356c5ffd1b6a3ea5f5cc9272038a7a874
IHaveQuestions2010 <petivagyok2010@gmail.com>
2025-06-24 23:14:34 +0200

    Can now create different scales and display them on the fretboard

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-selectedScale = "C"
+selected_scale = "C"
 
-baseTab = """e|---------------------------------------------------------------|
+base_tab = """e|---------------------------------------------------------------|
 B|---------------------------------------------------------------|
 G|---------------------------------------------------------------|
 D|---------------------------------------------------------------|
 A|---------------------------------------------------------------|
 E|---------------------------------------------------------------|"""
 
-allNotes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+ALL_NOTES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 
-majorScalesDict = {
+MAJOR_SCALES = {
     "C": ["C", "D", "E", "F", "G", "A", "B"], # All natural
 
     "G": ["G", "A", "B", "C", "D", "E", "F#"], # One sharp
     "D": ["D", "E", "F#", "G", "A", "B", "C#"], # Two sharps
-    "A":["A", "B", "C#", "D", "E", "F#", "G#"], # Three sharps
+    "A": ["A", "B", "C#", "D", "E", "F#", "G#"], # Three sharps
     "E": ["E", "F#", "G#", "A", "B", "C#", "D#"], # Four sharps
     "B": ["B", "C#", "D#", "E", "F#", "G#", "A#"], # Five sharps
     "F#": ["F#", "G#", "A#", "B", "C#", "D#", "E#", "F#"], # Six sharps
@@ -24,82 +24,59 @@ majorScalesDict = {
     "Bb": ["Bb", "C", "D", "Eb", "F", "G", "A"], # Two flats
     "Eb": ["Eb", "F", "G", "Ab", "Bb", "C", "D"], # Three flats
     "Ab": ["Ab", "Bb", "C", "Db", "Eb", "F", "G"], # Four flats
-    "Db":  ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"], # Five flats
+    "Db": ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"], # Five flats
     "Gb": ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"], # Six flats
     "Cb": ["Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb"] # Seven flats
 }
-#Probably won't be used
+
+# Probably won't be used
 '''
-minorScalesDict = {
-    "Am": majorScalesDict["C"], # All naturals
+MINOR_SCALES = {
+    "Am": MAJOR_SCALES["C"], # All naturals
 
-    "Em": majorScalesDict["G"], # One sharp
-    "Bm": majorScalesDict["D"], # Two sharps
-    "F#m": majorScalesDict["A"], # Three sharps
-    "C#m": majorScalesDict["E"], # Four sharps
-    "G#m": majorScalesDict["B"], # Five sharps
-    "D#m": majorScalesDict["F#"], # Six sharps
-    "A#m": majorScalesDict["C#"], # Seven sharps
+    "Em": MAJOR_SCALES["G"], # One sharp
+    "Bm": MAJOR_SCALES["D"], # Two sharps
+    "F#m": MAJOR_SCALES["A"], # Three sharps
+    "C#m": MAJOR_SCALES["E"], # Four sharps
+    "G#m": MAJOR_SCALES["B"], # Five sharps
+    "D#m": MAJOR_SCALES["F#"], # Six sharps
+    "A#m": MAJOR_SCALES["C#"], # Seven sharps
 
-    "Dm": majorScalesDict["F"], # One flat
-    "Gm": majorScalesDict["Bb"], # Two flats
-    "Cm": majorScalesDict["Eb"], # Three flats
-    "Fm": majorScalesDict["Ab"], # Four flats
-    "Bbm": majorScalesDict["Db"], # Five flats
-    "Ebm": majorScalesDict["Gb"], # Six flats
-    "Abm": majorScalesDict["Cb"] # Seven flats
+    "Dm": MAJOR_SCALES["F"], # One flat
+    "Gm": MAJOR_SCALES["Bb"], # Two flats
+    "Cm": MAJOR_SCALES["Eb"], # Three flats
+    "Fm": MAJOR_SCALES["Ab"], # Four flats
+    "Bbm": MAJOR_SCALES["Db"], # Five flats
+    "Ebm": MAJOR_SCALES["Gb"], # Six flats
+    "Abm": MAJOR_SCALES["Cb"] # Seven flats
 }
 '''
 
-def notesOnAString(string):
-    offset = allNotes.index(string)
-    notes = []
-    stringPosition = []
-    for note in range(12):
-        notes.append(allNotes[(note+offset)%12])
-    for note in notes:
-        stringPosition.append(notes.index(note))
-    return notes
+def get_notes_on_string(open_note):
+    """Return the 12 notes on a string starting from its open note."""
+    offset = ALL_NOTES.index(open_note)
+    return [ALL_NOTES[(i + offset) % 12] for i in range(12)]
 
-def noteInScale(scale, note):
-    if note in majorScalesDict[scale]:
-        return True
-    else:
-        return False
+def is_note_in_scale(scale, note):
+    """Check if a note is in the given major scale."""
+    return note in MAJOR_SCALES[scale]
 
+STRING_NAMES = ["E", "B", "G", "D", "A"]
 
-eString = notesOnAString("E")
-notesOnEString = []
-positionOnEString = []
-bString = notesOnAString("B")
-notesOnBString = []
-positionOnBString = []
-gString = notesOnAString("G")
-notesOnGString = []
-positionOnGString = []
-dString = notesOnAString("D")
-notesOnDString = []
-positionOnDString = []
-aString = notesOnAString("A")
-notesOnAString = []
-positionOnAString = []
+notes_per_string = [get_notes_on_string(name) for name in STRING_NAMES]
+notes_on_strings_in_scale = [[] for _ in STRING_NAMES]
+positions_on_strings_in_scale = [[] for _ in STRING_NAMES]
 
-strings = [eString, bString, gString, dString, aString]
-notesOnStrings = [notesOnEString, notesOnBString, notesOnGString, notesOnDString, notesOnAString]
-positionsOnStrings = [positionOnEString, positionOnBString, positionOnGString, positionOnDString, positionOnAString]
+for string_idx, string_notes in enumerate(notes_per_string):
+    for fret, note in enumerate(string_notes):
+        if is_note_in_scale(selected_scale, note):
+            notes_on_strings_in_scale[string_idx].append(note)
+            positions_on_strings_in_scale[string_idx].append(fret)
 
-note1 = []
-position = []
-noteInScalePlacment = [note1, position]
-
-for note in bString:
-    if noteInScale("C", note) == 1:
-        note1.append(note)
-        position.append(eString.index(note))
-
-
-for string in strings:
-    for note in string:
-        if noteInScale(selectedScale, note) == 1:
-            notesOnStrings[strings.index(string)].append(note)
-            positionsOnStrings[strings.index(string)].append(string.index(note))
+for string_idx, string_notes in enumerate(notes_per_string):
+    for fret, note in enumerate(string_notes):
+        if note in notes_on_strings_in_scale[string_idx]:
+            print(f"{fret}-", end="")
+        else:
+            print("x-", end="")
+    print("")
